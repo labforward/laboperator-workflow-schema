f13f02135eae537f76c58ebd61ad6a5b544c7a0a
kgrhartlage <webdevkonto@gmail.com>
2022-04-24 16:33:08 +0200

    [WFE-88] Add ESLint plugin for Jest

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,12 +1,19 @@
 module.exports = {
-  root: true,
   extends: '@labforward/eslint-config-node',
+  ignorePatterns: ['dist'],
+  overrides: [
+    {
+      files: ['**/__tests__/**/*.test.ts'],
+      plugins: ['jest'],
+      extends: ['plugin:jest/recommended'],
+    },
+  ],
   parserOptions: {
     requireConfigFile: false,
   },
+  root: true,
   rules: {
     // Allow importing devDependencies.
     'import/no-extraneous-dependencies': ['error', { devDependencies: true }],
   },
-  ignorePatterns: ['dist'],
 };
