9342990058affd0f449a18bed2bcedc45138bb68
Thrashattack <henrique202@gmail.com>
2023-05-12 12:21:07 +0200

    [CUB-4622] Adjust validation import in index

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,5 @@
-import validate from 'dist/validation';
-
 import type { Options, Validation } from './types';
+import validate from './validation';
 import workflowEventSchema from './workflow-event-schema.json';
 import workflowStepTemplateSchema from './workflow-step-template-schema.json';
 import workflowTemplateSchema from './workflow-template-schema.json';
