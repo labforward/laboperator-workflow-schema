1fb99b87f35919c2b3e395e90830ce28dcbd79bb
Ignatius Reza <lyoneil.de.sire@gmail.com>
2025-12-04 21:44:48 +0100

    yarn build

--- dist/index.js
+++ dist/index.js
@@ -4,11 +4,11 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.validate = exports.schemas = void 0;
-var validation_1 = __importDefault(require("./validation"));
+const validation_1 = __importDefault(require("./validation"));
 exports.validate = validation_1.default;
-var workflow_event_schema_json_1 = __importDefault(require("./workflow-event-schema.json"));
-var workflow_step_template_schema_json_1 = __importDefault(require("./workflow-step-template-schema.json"));
-var workflow_template_schema_json_1 = __importDefault(require("./workflow-template-schema.json"));
+const workflow_event_schema_json_1 = __importDefault(require("./workflow-event-schema.json"));
+const workflow_step_template_schema_json_1 = __importDefault(require("./workflow-step-template-schema.json"));
+const workflow_template_schema_json_1 = __importDefault(require("./workflow-template-schema.json"));
 exports.schemas = {
     workflowEventSchema: workflow_event_schema_json_1.default,
     workflowStepTemplateSchema: workflow_step_template_schema_json_1.default,
--- dist/validation.js
+++ dist/validation.js
@@ -3,41 +3,30 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
-var ajv_1 = __importDefault(require("ajv"));
-var ajv_formats_1 = __importDefault(require("ajv-formats"));
-var workflow_event_schema_json_1 = __importDefault(require("./workflow-event-schema.json"));
-var workflow_step_template_schema_json_1 = __importDefault(require("./workflow-step-template-schema.json"));
-var workflow_template_schema_json_1 = __importDefault(require("./workflow-template-schema.json"));
-var ajv = new ajv_1.default({
-    // Check all rules collecting all errors.
+const ajv_1 = __importDefault(require("ajv"));
+const ajv_formats_1 = __importDefault(require("ajv-formats"));
+const workflow_event_schema_json_1 = __importDefault(require("./workflow-event-schema.json"));
+const workflow_step_template_schema_json_1 = __importDefault(require("./workflow-step-template-schema.json"));
+const workflow_template_schema_json_1 = __importDefault(require("./workflow-template-schema.json"));
+const ajv = new ajv_1.default({
     allErrors: true,
-    // Allow using multiple non-null types in "type" keyword
     allowUnionTypes: true,
-    // Skip iterating over enumerable prototype properties.
     ownProperties: true,
-    // Include reference to the part of the schema and validated data in errors.
     verbose: true,
 });
-/**
- * Allow custom keyword that enables rich formatting for on hover documentation
- * in VS Code. See compilation script for implementation details.
- *
- * https://code.visualstudio.com/docs/languages/json#_use-rich-formatting-in-hovers
- */
 ajv.addKeyword({
     keyword: 'markdownDescription',
     schemaType: 'string',
 });
-// https://github.com/ajv-validator/ajv-formats
 (0, ajv_formats_1.default)(ajv);
-var validationFunctions = {
+const validationFunctions = {
     workflowEventTemplate: ajv.compile(workflow_event_schema_json_1.default),
     workflowStepTemplate: ajv.compile(workflow_step_template_schema_json_1.default),
     workflowTemplate: ajv.compile(workflow_template_schema_json_1.default),
 };
-var validateTemplate = function (data, validationFunction) {
-    var isValid = validationFunction(data);
-    var result = {
+const validateTemplate = (data, validationFunction) => {
+    const isValid = validationFunction(data);
+    const result = {
         errors: null,
         schema: validationFunction.schema,
     };
@@ -45,15 +34,6 @@ var validateTemplate = function (data, validationFunction) {
         result.errors = validationFunction.errors;
     return result;
 };
-/**
- * @param {?} data The data to validate against a specific schema.
- * @param {Options} [options.schema] The schema to validate against.
- *
- * @returns {Object} An object with the respective schema and validation errors.
- */
-var validate = function (data, _a) {
-    var _b = _a === void 0 ? {} : _a, _c = _b.schema, schema = _c === void 0 ? 'workflowTemplate' : _c;
-    return validateTemplate(data, validationFunctions[schema]);
-};
+const validate = (data, { schema = 'workflowTemplate' } = {}) => validateTemplate(data, validationFunctions[schema]);
 exports.default = validate;
 //# sourceMappingURL=validation.js.map
